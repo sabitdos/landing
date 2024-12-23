f715dc7880de5272db58575044d4022df2c2c43f
sabitdos <sabitdos@gmail.com>
2024-12-23 12:17:37 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-      src = "https://unpkg.com/vue@3/dist/vue.global.js" >
-    if ("serviceWorker" in navigator) {
-        self.addEventListener("load", async () => {
-            const container = navigator.serviceWorker;
-            if (container.controller === null) {
-                const reg = await container.register("sw.js");
-            }
-        });
-    }
+      src = "https://unpkg.com/vue@3/dist/vue.global.js"
